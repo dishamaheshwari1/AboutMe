b7c98f6036e9908afa7602869df99d99d190564a
Disha Maheshwari <disha.maheshwari@icloud.com>
2025-07-23 16:24:52 -0400

    tried to add scroll view

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(.systemBlue)
-                .ignoresSafeArea()
-            VStack(alignment: .leading, spacing: 20.0) {
-                
+        ScrollView {
+            ZStack {
+                Color(.blue1)
+                    .ignoresSafeArea()
+                VStack(alignment: .leading, spacing: 20.0) {
+                    Image("prom")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                    HStack(spacing: 20.0) {
+                        Text("Me")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("senior year prom")
+                    }
+                    Text("A day that felt like a whole week in one")
+                }
+                .padding()
+                .background(Rectangle() .foregroundColor(.white))
+                .cornerRadius(15)
+                .shadow(radius: 15)
+                .padding()
             }
         }
     }
